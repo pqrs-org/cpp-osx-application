8bb81bb954e651fce227b13b5a84dfede205228c
Takayama Fumihiko <tekezo@pqrs.org>
2026-03-21 14:30:31 +0900

    Version 1.1.0

--- include/pqrs/osx/application.hpp
+++ include/pqrs/osx/application.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::application v1.0
+// pqrs::osx::application v1.1
 
 // (C) Copyright Takayama Fumihiko 2026.
 // Distributed under the Boost Software License, Version 1.0.
